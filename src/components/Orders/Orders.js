96968b8c8ea9e0d61b5d68e41931268d50f35955
noormohammad20 <mdsaim204@gmail.com>
2022-03-31 01:59:17 +0600

    Implement Review Item style with a lot of flex

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import useCart from '../../hooks/useCart'
 import useProducts from '../../hooks/useProducts'
+import Cart from '../Cart/Cart'
+import ReviewItem from '../ReviewItem/ReviewItem'
 
 const Orders = () => {
     const [products, setProducts] = useProducts()
     const [cart, setCart] = useCart(products)
     return (
-        <div>
-            <h2>{products.length}</h2>
-            <p>Cart has {cart.length}</p>
+        <div className='shop-container'>
+            <div className='review-items-container'>
+                {
+                    cart.map(product =>
+                        <ReviewItem
+                            key={product.id}
+                            product={product}
+                        ></ReviewItem>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     )
 }

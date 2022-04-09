8671196bb87a2cc9193097613a22618a40d0379f
noormohammad20 <mdsaim204@gmail.com>
2022-04-09 21:49:41 +0600

    create login and signup component and set the route for component and create and style login page

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SignUp.css'
+const SignUp = () => {
+    return (
+        <div>
+            <h2>This is signup</h2>
+        </div>
+    )
+}
+
+export default SignUp

451395453cc9c06b2099d888831fd2d07bed2ae2
noormohammad20 <mdsaim204@gmail.com>
2022-04-10 01:59:34 +0600

    added some steps

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,11 +1,15 @@
 /**
- * 1. create a new project in console.firebase.google.com
+ * 1. create a new firebase project in console.firebase.google.com
  * 2. npm install firebase
  * 3. create firebase.init.js and import getAuth to export auth
  * 4. firebase settings > Authentication > enable Email and Password Auth
  * 5. create login ,signUp component  , and set route
- * 6. attach onBlur in input and create function for get value from input
- * 7. 
+ * 6. attach onBlur in input and create function for get value from     input
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword() call form react firebase hooks
+ * 9. if user is created redirect to the expected page
+ * 10. useSignInWithEmailAndPassword() for SignIn
+ * 11. 
 */
 
 

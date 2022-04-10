0f4d15af5e9fae8769ddc87b0415b25f85b7d856
noormohammad20 <mdsaim204@gmail.com>
2022-04-10 18:54:18 +0600

    added some steps

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -19,8 +19,9 @@
 
 /**
  * firebase hosting steps 
- * 1. npm install -g firebase-tools
- * 2. firebase login
- * 3. firebase init 
- * 4. firebase deploy
+ * 1. npm install -g firebase-tools (for one time for your pc)
+ * 2. firebase login   (for one time for your pc)
+ * 3. firebase init   (for each project one time)
+ * 4. npm run build (every time you want to deploy)
+ * 5. firebase deploy  (every time you want to deploy)
  */ 

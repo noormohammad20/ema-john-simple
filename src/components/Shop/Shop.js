10991765161b16f6a1b5ceffabf34db5b52ce872
noormohammad20 <mdsaim204@gmail.com>
2022-04-24 11:46:43 +0600

    paigination button created

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,6 +10,17 @@ import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useProducts()
     const [cart, setCart] = useState([])
+    const [pageCount, setPageCount] = useState(0)
+
+    useEffect(() => {
+        fetch('http://localhost:5000/productCount')
+            .then(res => res.json())
+            .then(data => {
+                const count = data.count
+                const pages = Math.ceil(count / 10)
+                setPageCount(pages)
+            })
+    })
 
 
     useEffect(() => {
@@ -53,6 +64,12 @@ const Shop = () => {
                         handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
+                <div className='pagination'>
+                    {
+                        [...Array(pageCount).keys()]
+                            .map(number => <button>{number + 1}</button>)
+                    }
+                </div>
             </div>
 
             <div className="cart-container">
